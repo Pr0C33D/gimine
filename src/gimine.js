b80c5bdfeb2baf69af4c6097e0d660c43e72a417
Pr0C33D <77962630+Pr0C33D@users.noreply.github.com>
2021-10-13 21:11:16 +0300

    update of medis new task

--- src/gimine.js
+++ src/gimine.js
@@ -2,10 +2,82 @@ const gimine = [
     {
         name: 'Petras',
         age: 99,
+        children: [
+            {
+                name: "Margaret",
+                age: '87',
+                children: [],
+            }
+        ]
     },
     {
         name: 'Maryte',
         age: 102,
+        children: [
+            {
+                name: "Saulius",
+                age: '17',
+                children: [],
+            }
+        ]
+    },
+    {
+        name: 'Ona',
+        age: 50,
+        children: [
+            {
+                name: "Edvinas",
+                age: '67',
+                children: [
+                    {
+                        name: 'Tadas',
+                        age: 41,
+                        children: [
+                            {
+                                name: 'Luisa',
+                                age: 1000,
+                                children: [],
+                            }
+                        ],
+                    }
+                ],
+            }
+        ]
+    },
+    {
+        name: 'Kazys',
+        age: 77,
+        children: [
+            {
+                name: "Klajunas",
+                age: '57',
+                children: [
+                    {
+                        name: "Albertas",
+                        age: '88',
+                        children: [
+                            {
+                                name: "Leonardas",
+                                age: '57',
+                                children: [
+                                    {
+                                        name: 'Donatelas',
+                                        age: 77,
+                                        children: [
+                                            {
+                                                name: 'Leonardas',
+                                                age: 72,
+                                                children: [],
+                                            }
+                                        ],
+                                    }
+                                ],
+                            }
+                        ],
+                    }
+                ],
+            }
+        ]
     },
 ];
 
